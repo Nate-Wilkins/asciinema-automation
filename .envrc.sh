68a0e7f21d2df8dd2f6b84d74a9449e8e640a1f3
nate-wilkins <nate-wilkins@code-null.com>
2024-04-02 07:07:02 -0400

    Update '.envrc.sh'

--- .envrc.sh
+++ .envrc.sh
@@ -10,6 +10,8 @@ function develop() {
     --experimental-features 'nix-command flakes' \
     --show-trace \
     --verbose \
+    --option max-jobs 8 \
+    --option cores 2 \
     # NOTE: asciinema requires X configuration.
     # --ignore-environment \
     "."
